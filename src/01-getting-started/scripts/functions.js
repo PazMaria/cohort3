cf414e6a06020dda255eed48a60d7a9713d28d0a
Maria Paz <pazguada@gmail.com>
2019-10-08 15:46:53 -0600

    exercise-enhancement

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,21 +1,19 @@
-
 const functions = {
-    
-    size: (num) => {
-        if (num < 10) return "small";
-        if (num < 20) return "medium";
-        return "large";
-    },
+  size: num => {
+    if (num < 0) return "negative";
+    if (num < 10) return "small";
+    if (num < 20) return "medium";
+    if (num > 100) return "extra large";
+    return "large";
+  },
 
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
 
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    }
-
-    
+  subtract: (num1, num2) => {
+    return num1 - num2;
+  }
 };
 
 export default functions;
--- src/01-getting-started/scripts/functions.test.js
+++ src/01-getting-started/scripts/functions.test.js
@@ -1,15 +1,16 @@
-import functions from './functions'
+import functions from "./functions";
 
-test('Check the sizes', () => {
-    expect(functions.size(-1)).toBe("small"); // Consider the edge cases
-    expect(functions.size(0)).toBe("small");
-    expect(functions.size(10)).toBe("medium");
-    expect(functions.size(15)).toBe("medium");
-    expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+test("Check the sizes", () => {
+  expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
+  expect(functions.size(0)).toBe("small");
+  expect(functions.size(10)).toBe("medium");
+  expect(functions.size(15)).toBe("medium");
+  expect(functions.size(20)).toBe("large");
+  expect(functions.size(2000000)).toBe("extra large");
+  expect(functions.size(-101)).toBe("negative");
 });
 
-test('Does that add function work?', () => {
-    expect(functions.add(1,2)).toBe(3);
-    expect(functions.add(101,202)).toBe(303);
+test("Does that add function work?", () => {
+  expect(functions.add(1, 2)).toBe(3);
+  expect(functions.add(101, 202)).toBe(303);
 });
