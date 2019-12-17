3ce308ff9bbef9d32ac4180e20099da58a9102e4
Maria Paz <pazguada@gmail.com>
2019-12-17 14:13:19 -0700

    Added first and last position functionality

--- react-02/src/components/linkedLists/scripts/linkedList.js
+++ react-02/src/components/linkedLists/scripts/linkedList.js
@@ -13,10 +13,13 @@ class ListNode {
 class LinkedList {
   constructor() {
     this.head = null;
+    this.position = 0;
   }
 
   insertNode(subject, amount) {
+    this.position++;
     const newNode = new ListNode(subject, amount);
+    newNode.position = this.position;
     if (this.head === null) {
       this.head = newNode;
       return this.head;
@@ -27,6 +30,22 @@ class LinkedList {
       }
       lastNode.forwardNode = newNode;
       return this.head;
+    }
+  }
+
+  firstPosition() {
+    return `${this.head.subject} Position: ${this.head.position}`;
+  }
+
+  lastPosition() {
+    if (this.head === null) {
+      return "List is empty";
+    } else {
+      let lastNode = this.head;
+      while (lastNode.forwardNode !== null) {
+        lastNode = lastNode.forwardNode;
+      }
+      return `${lastNode.subject} Position: ${lastNode.position}`;
     }
   }
 }
--- react-02/src/components/linkedLists/scripts/linkedList.test.js
+++ react-02/src/components/linkedLists/scripts/linkedList.test.js
@@ -12,3 +12,17 @@ test("insert node", () => {
   link.insertNode("Potatoes", 6);
   expect(link.head.forwardNode.subject).toBe("Potatoes");
 });
+
+test("check first position", () => {
+  const link = new LinkedList();
+  link.insertNode("Apples", 5);
+  link.insertNode("Potatoes", 6);
+  expect(link.firstPosition()).toBe("Apples Position: 1");
+});
+
+test("check last position", () => {
+  const link = new LinkedList();
+  link.insertNode("Apples", 5);
+  link.insertNode("Potatoes", 6);
+  expect(link.lastPosition()).toBe("Potatoes Position: 2");
+});
