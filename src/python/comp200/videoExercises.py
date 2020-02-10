bb72cc8daf24405e7c1404202acb2f8ec68d832b
Maria Paz <pazguada@gmail.com>
2020-02-10 10:24:38 -0700

    Exercise 2

--- src/python/comp200/videoExercises.py
+++ src/python/comp200/videoExercises.py
@@ -5,3 +5,18 @@ var2 = "My variable"
 
 num1 = 2
 num2 = 8
+
+
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+
+my_list = [65, 10, 25]
+
+# Create a tuple, called my_tuple, with a single value in it
+
+my_tuple = (5,)
+
+# Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 77, 9, 12}
+print(set1.intersection(set2))
